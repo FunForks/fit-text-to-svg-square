04b7fcaf1d5cb17a1d88413c599af3b40dd6d882
James Newton <blackslate@lexogram.com>
2024-05-26 10:51:53 +0300

    Add input to set SVG text

--- script.js
+++ script.js
@@ -4,27 +4,34 @@
 ;(function (){
   const text = document.getElementsByTagName("text")[0]
   const outline = document.getElementById("outline")
-  const rect = text.getBoundingClientRect()
-  console.log("rect:", rect);
+  const input = document.getElementsByTagName("input")[0]
 
-  const bBox = text.getBBox()
-  const { x, y, width, height } = bBox
-  console.log("bBox:", bBox);
-
-  const yNow = text.getAttribute("y")
-  const gap = (100 - height) / 2
-  const adjustY = y - gap
-  const newY = yNow - adjustY
-  text.setAttribute("y", newY)
-
-  ;(function (){
+  function resizeToFit() {
+    text.textContent = input.value
+  
     const bBox = text.getBBox()
     const { x, y, width, height } = bBox
+    console.log("bBox:", bBox);
 
-    outline.setAttribute("x", x)
-    outline.setAttribute("y", y)
-    outline.setAttribute("width", width)
-    outline.setAttribute("height", height)
-  })()
+    const yNow = text.getAttribute("y")
+    const gap = (100 - height) / 2
+    const adjustY = y - gap
+    const newY = yNow - adjustY
+    text.setAttribute("y", newY)
+
+    ;(function (){
+      const bBox = text.getBBox()
+      const { x, y, width, height } = bBox
+
+      outline.setAttribute("x", x)
+      outline.setAttribute("y", y)
+      outline.setAttribute("width", width)
+      outline.setAttribute("height", height)
+    })()
+  }
+
+  input.addEventListener("keyup", resizeToFit)
+
+  resizeToFit()
 })()
 
