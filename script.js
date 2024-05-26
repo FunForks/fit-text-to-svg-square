79231e174794497e4e141083396b22468057f82b
James Newton <blackslate@lexogram.com>
2024-05-26 10:54:27 +0300

    Centre text horizontally

--- script.js
+++ script.js
@@ -8,16 +8,26 @@
 
   function resizeToFit() {
     text.textContent = input.value
+    let size = text.getAttribute("font-size")
+    console.log("size:", size);
+    let delta = size / 2
+    
   
     const bBox = text.getBBox()
     const { x, y, width, height } = bBox
     console.log("bBox:", bBox);
 
     const yNow = text.getAttribute("y")
-    const gap = (100 - height) / 2
-    const adjustY = y - gap
+    const yGap = (100 - height) / 2
+    const adjustY = y - yGap
     const newY = yNow - adjustY
     text.setAttribute("y", newY)
+
+    const xNow = text.getAttribute("x")
+    const xGap = (100 - width) / 2
+    const adjustX = x - xGap
+    const newX = xNow - adjustX
+    text.setAttribute("x", newX)
 
     ;(function (){
       const bBox = text.getBBox()
